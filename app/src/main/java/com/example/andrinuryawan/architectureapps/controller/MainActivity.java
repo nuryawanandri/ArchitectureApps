905ab82ebddcbc48e9471dc8343bcd59397a3241
andri nuryawan <andri@skyshi.com>
2018-11-27 11:32:03 +0700

    add branch mvc

--- app/src/main/java/com/example/andrinuryawan/architectureapps/controller/MainActivity.java
+++ app/src/main/java/com/example/andrinuryawan/architectureapps/controller/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.andrinuryawan.architectureapps.view;
+package com.example.andrinuryawan.architectureapps.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
